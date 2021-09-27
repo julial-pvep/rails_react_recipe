f91db8b0a379fe0f2ec2c4a374741ca341bb4523
Julia Les <julial@pvep.com>
2021-09-27 16:37:43 -0400

    Fix typos

--- app/javascript/packs/Index.jsx
+++ app/javascript/packs/Index.jsx
@@ -3,7 +3,7 @@ import { render } from "react-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import $ from 'jquery';
 import Popper from 'popper.js';
-import 'bootstrap/dist/css/bootstrap.bundle.min';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 import App from "../components/App";
 
 document.addEventListener("DOMContentLoaded", () => {
--- app/javascript/routes/Index.jsx
+++ app/javascript/routes/Index.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BroweserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "../components/Home";
 
 export default (
